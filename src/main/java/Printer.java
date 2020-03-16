106fd8fdaa5a431844b19c2b083781378785c48c
Emma Roberts <emma.roberts197@gmail.com>
2020-03-16 17:03:30 +0000

    Finished extensions

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,17 +1,20 @@
 public class Printer {
 
     private int sheetsLeft;
+    private int toner;
 
-    public Printer(int sheetsLeft){
+    public Printer(int sheetsLeft, int toner){
         this.sheetsLeft = sheetsLeft;
+        this.toner = toner;
     }
 
     public String print(int pages, int copies){
-        if (this.sheetsLeft > (pages * copies)){
+        if (this.sheetsLeft > (pages * copies) && (this.toner > 0)){
             this.sheetsLeft = this.sheetsLeft - (pages * copies);
-            return "Success! You have " + this.sheetsLeft + " pages left!";
+            this.toner = this.toner - (pages * copies);
+            return "Success! You have " + this.sheetsLeft + " pages left! Your toner levels are at: " + this.toner;
         } else {
-            return "Add more paper to the paper tray!";
+            return "Add more paper to the paper tray or replace your toner!";
         }
     }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -9,16 +9,16 @@ public class PrinterTest {
 
     @Before
     public void before(){
-        printer = new Printer(100);
+        printer = new Printer(100, 100);
     }
 
     @Test
     public void testCanPrint(){
-        assertEquals("Success! You have 50 pages left!", printer.print(25, 2));
+        assertEquals("Success! You have 50 pages left! Your toner levels are at: 50", printer.print(25, 2));
     }
 
     @Test
     public void testCannotPrint(){
-        assertEquals("Add more paper to the paper tray!", printer.print(200, 1));
+        assertEquals("Add more paper to the paper tray or replace your toner!", printer.print(200, 1));
     }
 }
